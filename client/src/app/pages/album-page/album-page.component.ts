a21b48f12cdc501d3862409b246af13ac1c81111
Austin Tsang <168361948+aktsang2@users.noreply.github.com>
2025-10-28 20:58:34 -0700

    Update album-page.component.ts

--- client/src/app/pages/album-page/album-page.component.ts
+++ client/src/app/pages/album-page/album-page.component.ts
@@ -3,24 +3,33 @@ import { ActivatedRoute } from '@angular/router';
 import { ArtistData } from '../../data/artist-data';
 import { TrackData } from '../../data/track-data';
 import { AlbumData } from '../../data/album-data';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
-    selector: 'app-album-page',
-    templateUrl: './album-page.component.html',
-    styleUrls: ['./album-page.component.css'],
-    standalone: false
+  selector: 'app-album-page',
+  templateUrl: './album-page.component.html',
+  styleUrls: ['./album-page.component.css'],
+  standalone: false
 })
 export class AlbumPageComponent implements OnInit {
-	albumId:string;
-	album:AlbumData;
-	tracks:TrackData[];
+  albumId: string;
+  album: AlbumData;
+  tracks: TrackData[];
 
-
-  constructor(private route: ActivatedRoute) { }
+  // ✅ Inject SpotifyService
+  constructor(private route: ActivatedRoute, private spotifyService: SpotifyService) {}
 
   ngOnInit() {
     this.albumId = this.route.snapshot.paramMap.get('id');
-    //TODO: inject spotifyService and use it to get the album data and the tracks for the album
-  }
 
+    // ✅ Fetch album details
+    this.spotifyService.getAlbum(this.albumId).subscribe((data: any) => {
+      this.album = new AlbumData(data);
+    });
+
+    // ✅ Fetch album tracks
+    this.spotifyService.getAlbumTracks(this.albumId).subscribe((data: any) => {
+      this.tracks = data.items.map((t: any) => new TrackData(t));
+    });
+  }
 }
