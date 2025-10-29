910d3206d6572f6968f9f62b507167c87a64e018
Austin Tsang <168361948+aktsang2@users.noreply.github.com>
2025-10-28 21:55:21 -0700

    Update search.component.ts

--- client/src/app/components/search/search.component.ts
+++ client/src/app/components/search/search.component.ts
@@ -1,34 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SpotifyService } from '../../services/spotify.service';
-import { ArtistData } from '../../data/artist-data';
-import { AlbumData } from '../../data/album-data';
-import { TrackData } from '../../data/track-data';
 import { ResourceData } from '../../data/resource-data';
 
 @Component({
-    selector: 'app-search',
-    templateUrl: './search.component.html',
-    styleUrls: ['./search.component.css'],
-    providers: [SpotifyService],
-    standalone: false
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css'],
+  standalone: false
 })
-export class SearchComponent implements OnInit {
-  searchString:string;
-  searchCategory:string = 'artist';
-  searchCategories:string[] = ['artist', 'album', 'track'];
-  resources:ResourceData[];
+export class SearchComponent {
+  searchString: string = '';
+  searchCategory: string = 'artist';
+  searchCategories: string[] = ['artist', 'album', 'track'];
+  results: ResourceData[] = [];
 
-  constructor(private spotifyService:SpotifyService) { }
+  constructor(private spotifyService: SpotifyService) {}
 
-  ngOnInit() {
-  }
-
-results: ResourceData[] = [];
-
-doSearch() {
-  this.spotifyService.searchFor(this.searchCategory, this.searchString).then((data) => {
-    this.results = data;
+  doSearch() {
+    this.spotifyService.searchFor(this.searchCategory, this.searchString).then((data) => {
+      this.results = data;
     });
-   }
- }
+  }
 }
+
